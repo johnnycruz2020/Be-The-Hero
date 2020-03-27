2e3e4d8e5fc2f8dbf560acc6caeda1c5868c7904
Johnny Cruz <johnny@vermais.com>
2020-03-27 20:08:24 -0300

    Funcionalidades Avançadas da Aplicação

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,11 +1,11 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/genarteUniqueId');
 const connection = require('../database/connection');
 
 module.exports ={
   async store(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
